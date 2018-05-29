b98bacc59fa4226afefb71f1751dd38802b6d729
Trung Nguyen <trungn@axon.com>
2018-05-29 17:26:19 +0700

    upgrade dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object Dependencies {
 
   val commonDependencies = {
-    val scalaTestVersion = "2.2.5"
+    val scalaTestVersion = "3.0.5"
     Seq(
       "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
     )
   }
 
   val akkaDependencies = {
-    val akkaVersion = "2.4.14"
+    val akkaVersion = "2.5.12"
     Seq(
       "com.typesafe.akka" %% "akka-actor"   % akkaVersion,
       "com.typesafe.akka" %% "akka-slf4j"   % akkaVersion,
@@ -19,25 +19,25 @@ object Dependencies {
   }
 
   val akkaHttpDependencies = {
-    val akkaHttpVersion = "10.0.0"
-    val json4sVersion = "3.3.0"
+    val akkaHttpVersion = "10.1.1"
+    val json4sVersion = "3.5.4"
     Seq(
       "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
       "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % "test",
 
       "org.json4s" %% "json4s-jackson" % json4sVersion,
       "org.json4s" %% "json4s-ext" % json4sVersion,
-      "de.heikoseeberger" %% "akka-http-json4s" % "1.11.0"
+      "de.heikoseeberger" %% "akka-http-json4s" % "1.21.0"
     )
   }
 
   val playDependencies = Seq(
-    "org.scalatestplus" %% "play" % "1.4.0" % "test"
+    "org.scalatestplus" %% "play" % "3.1.0" % "test"
   )
 
   val gatlingDependencies = Seq(
-    "io.gatling.highcharts" % "gatling-charts-highcharts" % "2.1.7" % "test",
-    "io.gatling"            % "gatling-test-framework"    % "2.1.7" % "test"
+    "io.gatling.highcharts" % "gatling-charts-highcharts" % "2.3.0" % "test",
+    "io.gatling"            % "gatling-test-framework"    % "2.3.0" % "test"
   )
 }
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,3 @@
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.4.6")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.15")
 
-addSbtPlugin("io.gatling" % "gatling-sbt" % "2.1.7")
+addSbtPlugin("io.gatling" % "gatling-sbt" % "2.2.2")
